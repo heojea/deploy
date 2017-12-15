761f361d0dbda729d0abaf4f499d4cc7c7111ce8
heojea <heojea@192.168.25.58>
2017-12-16 00:42:54 +0900

    WEFDSFDSF

---
+++ src/test/java/com/kftc/jettyRest/jettyRestClientTest.java
@@ -0,0 +1,92 @@
+package com.kftc.jettyRest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.junit.Test;
+
+public class jettyRestClientTest {
+
+	@Test
+	public void clientGetType() {
+	  try {
+
+		DefaultHttpClient httpClient = new DefaultHttpClient();
+		HttpGet getRequest = new HttpGet(
+			"http://localhost:9090/say/hello");
+		getRequest.addHeader("accept", "application/json");
+
+		HttpResponse response = httpClient.execute(getRequest);
+
+		if (response.getStatusLine().getStatusCode() != 200) {
+			throw new RuntimeException("Failed : HTTP error code : "
+			   + response.getStatusLine().getStatusCode());
+		}
+
+		BufferedReader br = new BufferedReader(
+                         new InputStreamReader((response.getEntity().getContent())));
+
+		String output;
+		System.out.println("Output from Server .... \n");
+		while ((output = br.readLine()) != null) {
+			System.out.println(output);
+		}
+
+		httpClient.getConnectionManager().shutdown();
+
+	  } catch (ClientProtocolException e) {
+
+		e.printStackTrace();
+
+	  } catch (IOException e) {
+
+		e.printStackTrace();
+	  }
+
+	}
+
+	
+	@Test
+	public void clientPostType() {
+	  try {
+
+			DefaultHttpClient httpClient = new DefaultHttpClient();
+			HttpPost postRequest = new HttpPost(
+				"http://localhost:8080/RESTfulExample/json/product/post");
+
+			StringEntity input = new StringEntity("{\"qty\":100,\"name\":\"iPad 4\"}");
+			input.setContentType("application/json");
+			postRequest.setEntity(input);
+
+			HttpResponse response = httpClient.execute(postRequest);
+
+			if (response.getStatusLine().getStatusCode() != 201) {
+				throw new RuntimeException("Failed : HTTP error code : "
+					+ response.getStatusLine().getStatusCode());
+			}
+
+			BufferedReader br = new BufferedReader(
+	                        new InputStreamReader((response.getEntity().getContent())));
+
+			String output;
+			System.out.println("Output from Server .... \n");
+			while ((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+			httpClient.getConnectionManager().shutdown();
+		  } catch (MalformedURLException e) {
+			e.printStackTrace();
+		  } catch (IOException e) {
+			e.printStackTrace();
+		  }
+
+	}
+}
