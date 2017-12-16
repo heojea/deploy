a3d1350c74215228f9649c1066404b9e2fd844f6
heojea <heojea@192.168.25.62>
2017-12-16 18:48:57 +0900

    ddd

--- src/main/java/co/kr/jettyRest/process/HelloProcess.java
+++ src/main/java/co/kr/jettyRest/process/HelloProcess.java
@@ -1,23 +1,67 @@
 package co.kr.jettyRest.process;
 
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.camel.spi.DataFormat;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 /**
  * Created by v.jyheo on 2017-12-15.
  * 설명 : exchange 사용방법을 최대한 기술 하도록 한다.
  *       http rest 기반의 값들을 어떻게 가져오는지 확일 할수 있도록 한다.
  */
-public class HelloProcess implements Processor {
+public class HelloProcess implements Processor , DataFormat{
+	private ObjectMapper jacksonMapper;
+	  
     public void process(Exchange exchange) throws Exception {
+    	jacksonMapper = new ObjectMapper();
         System.out.println("exchange.toString():::"+exchange.toString());
         System.out.println("exchange.getIn().toString():::"+exchange.getIn().toString());
         System.out.println("exchange.getIn().getHeaders().toString():::"+exchange.getIn().getHeader("Accept"));
         System.out.println("exchange.getIn().getHeaders().toString():::"+exchange.getIn().getHeader("Accept-Language"));
         System.out.println("exchange.getIn().getHeaders().toString():::"+exchange.getIn().getHeader("Cache-Control"));
+        System.out.println("exchange.getProperty(viewClass):::"+exchange.getProperty("viewClass"));
+        System.out.println("exchange.getOut().getBody(OutputStream.class):::" + exchange.getIn().getBody(OutputStream.class));
+        System.out.println("exchange.getIn().getHeader(Exchange.HTTP_SERVLET_RESPONSE):::" + exchange.getIn().getHeader(Exchange.HTTP_SERVLET_RESPONSE));
+        
+        HttpServletResponse response = (HttpServletResponse) exchange.getIn().getHeader(Exchange.HTTP_SERVLET_RESPONSE);
+        
+        //exchange.getOut().setBody("{\"id\":2,\"content\":\"Hello, User!\"}");
+        //String responseString = "{\"id\":2,\"content\":\"Hello, User!\"}";
+        
+        Map<String,String> payload = new HashMap<>();
+        payload.put("id","2");
+        payload.put("content","User");
 
-        exchange.getOut().setBody("{aaa,aabvcd}");
+        //String json = new ObjectMapper().writeValueAsString(payload);
+        this.marshal(exchange, payload , response.getOutputStream());
+        
+    }
+
+	@Override
+	public void marshal(Exchange exchange, Object obj, OutputStream stream) throws Exception {
+        Class view = (Class) exchange.getProperty("viewClass");
+        if (view != null){
+            ObjectWriter w = jacksonMapper.writerWithView(view);
+            w.writeValue(stream, obj);
+        }else{
+            stream.write(jacksonMapper.writeValueAsBytes(obj));
+        }
 
     }
+
+	@Override
+	public Object unmarshal(Exchange arg0, InputStream arg1) throws Exception {
+		return null;
+	}
 }
