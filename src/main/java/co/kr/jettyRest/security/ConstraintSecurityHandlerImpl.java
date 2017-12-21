e05c17259722685c811c548f07a530ca94d98454
heojea <heojea@gmail.com>
2017-12-21 15:29:29 +0900

    테스트 프로젝트 commit http rest server

--- src/main/java/co/kr/jettyRest/security/ConstraintSecurityHandlerImpl.java
+++ src/main/java/co/kr/jettyRest/security/ConstraintSecurityHandlerImpl.java
@@ -11,6 +11,13 @@ import java.util.Iterator;
  */
 public class ConstraintSecurityHandlerImpl extends ConstraintSecurityHandler {
 
+    /**
+     * 이상하게 외부에서 설정해 놓은 주입한 LoginService 주석처리 하고 무조건 아래의 사용자 정의 인증을 거치도록 바꿧다.
+     * 이유는 알수 없지만 중복하여 인스턴스를 주입시키는것으로 보인다.
+     * @return
+     * @throws Exception
+     */
+    @Override
     protected LoginService findLoginService() throws Exception {
         LoginService service = new HardcodedLoginService();
         return service;
