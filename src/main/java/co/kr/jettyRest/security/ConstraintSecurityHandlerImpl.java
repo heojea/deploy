90377e297c3570b9a42f51d4a48b67fac30e5ed1
heojea <heojea@gmail.com>
2017-12-21 15:48:35 +0900

    테스트 프로젝트 commit http rest server

--- src/main/java/co/kr/jettyRest/security/ConstraintSecurityHandlerImpl.java
+++ src/main/java/co/kr/jettyRest/security/ConstraintSecurityHandlerImpl.java
@@ -2,6 +2,8 @@ package co.kr.jettyRest.security;
 
 import org.eclipse.jetty.security.ConstraintSecurityHandler;
 import org.eclipse.jetty.security.LoginService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 import java.util.Iterator;
@@ -10,7 +12,6 @@ import java.util.Iterator;
  * Created by v.jyheo on 2017-12-21.
  */
 public class ConstraintSecurityHandlerImpl extends ConstraintSecurityHandler {
-
     /**
      * 이상하게 외부에서 설정해 놓은 주입한 LoginService 주석처리 하고 무조건 아래의 사용자 정의 인증을 거치도록 바꿧다.
      * 이유는 알수 없지만 중복하여 인스턴스를 주입시키는것으로 보인다.
